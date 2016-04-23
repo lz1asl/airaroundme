5314b517e7db62f7fa164117af7fbb852dced8b4
Ivo <harbachek@gmail.com>
2016-04-23 19:47:31 +0300

    Added symptoms dropdown

--- public/js/form.js
+++ public/js/form.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+    // Load symptom dropdown options
+    $.ajax({
+        url: 'symptom',
+        type: 'GET',
+        dataType: 'json',
+        success: function (json) {
+            $.each(json, function (i, value) {
+                $('#symptoms').append($('<option>').text(value.label).attr('value', value.id));
+            });
+        },
+        failure: function (json) {
+            console.error('Cannot load the symptoms options: ' + JSON.parse(json))
+        }
+    });
+
+    // Load severity dropdown options
     $.ajax({
         url: 'severities',
         type: 'GET',
