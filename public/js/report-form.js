38e0990d38275c88eff5795bf407e5694b04026e
Ivo <harbachek@gmail.com>
2016-04-24 12:05:11 +0300

    Showing notification will hide

--- public/js/report-form.js
+++ public/js/report-form.js
@@ -48,8 +48,11 @@ $(document).ready(function() {
             data: JSON.stringify(body),
             contentType: "application/json",
             success: function(result) {
-                $("#report-form").find("input, textarea").val("");
+                $("#report-form").find("input, textarea, select").val("");
                 $("#notifcation-area").show();
+                setTimeout(function() {
+                    $("#notifcation-area").hide();
+                }, 2000);
             }
         });
     });
