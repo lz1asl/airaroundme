8d56deca52d487f5e7d9a999881e807dde95bb11
lz1asl <alexander.stoev@gmail.com>
2016-04-24 08:52:03 +0100

    import climate records data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,43 @@ Sympthom.create(label: 'Other')
 Severity.create(id: 1, label: 'Low')
 Severity.create(id: 2, label: 'Medium')
 Severity.create(id: 3, label: 'High')
+
+
+def createReport(marker, reporttype)
+  # e.g. "Cyclone Kerry : Largest-Eye 93.3 km (56 mi) on 21/2/1979
+  name =  marker.xpath("locationName/text()").to_s
+  value =  marker.xpath("/value/text()").to_s
+  record = marker.xpath("recordCharacteristic/text()").to_s
+  date =  marker.xpath("measureDate/text()").to_s
+  label = name + ': ' +  record + ' ' + value + ' on ' + date
+  puts label
+
+  Report.create(
+      lat: marker['lat'],
+      lon: marker['lng'],
+      reporttype: reporttype,
+      note: label
+  )
+end
+
+def importFile(file, reporttype)
+  markers = Nokogiri::XML(File.open("public/sampledata/" + file)) do |config|
+    config.strict.nonet
+  end
+  markers.xpath("//markers/marker").each do |marker|
+    createReport(marker, 'reporttype')
+  end
+end
+
+importFile("cyclone.xml", "cyclone")
+importFile("rainfall.xml", "rainfall")
+
+importFile("temperature.xml", "temperature")
+importFile("tornado.xml", "tornado")
+importFile("wave.xml", "wave")
+importFile("wind.xml", "wind")
+
+
+
+
+
